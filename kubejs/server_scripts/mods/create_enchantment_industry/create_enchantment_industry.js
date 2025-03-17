0ae8b329d0d49d74c90753980128d62e4ec3612e
goldySlav <g00L1v3rjkE@proton.me>
2025-03-17 20:03:23 +0100

    experience_rotor rebalance

--- kubejs/server_scripts/mods/create_enchantment_industry/create_enchantment_industry.js
+++ kubejs/server_scripts/mods/create_enchantment_industry/create_enchantment_industry.js
@@ -7,4 +7,10 @@ ServerEvents.recipes((event) => {
 	
 	event.shapeless(CR('item_drain'), CR_EI("disenchanter"))
 	event.shapeless(CR_EI('disenchanter'), CR("item_drain"))
+	
+	//experience motor
+	removeRecipeByOutput(event, [
+		CR_EI('experience_motor'),
+	])
+    donutCraftFull(event, CR_EI("experience_rotor"), CR("propeller"), CR('experience_nugget'))
 })
