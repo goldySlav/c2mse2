de9cab88755e8811cbffb415bde30355e8f2c370
goldySlav <g00L1v3rjkE@proton.me>
2025-04-07 12:42:28 +0200

    change nether portal requirements: netherrack, blaze powder

--- kubejs/startup_scripts/custom_portals.js
+++ kubejs/startup_scripts/custom_portals.js
@@ -7,25 +7,38 @@ const $Item = Java.loadClass('net.minecraft.world.item.Item')
 const $Block = Java.loadClass('net.minecraft.world.level.block.Block')
 
 StartupEvents.postInit(event => {
-  let frameBlock = $ForgeRegistries.BLOCKS.getValue(new $ResourceLocation("create:andesite_casing"))
-  let lightItem = $ForgeRegistries.ITEMS.getValue(new $ResourceLocation("kubejs:kinetic_mechanism"))
+	function customPortal(frameBlockId, lightItemId, dimDestination, dimReturn, tintColorObj) {
+		
+		const frameBlock = $ForgeRegistries.BLOCKS.getValue(new $ResourceLocation(frameBlockId))
+		const lightItem = $ForgeRegistries.ITEMS.getValue(new $ResourceLocation(lightItemId))
 
-  if (!frameBlock || !lightItem) {
-    console.error("Frame block or light item not found!")
-    return
-  }
+		if (!frameBlock || !lightItem) {
+			console.error("Frame block or light item not found!")
+			return
+		}
 
-  let builder = $CustomPortalBuilder.beginPortal()
+		const builder = $CustomPortalBuilder.beginPortal()
 
-  // Force correct overloads
-  builder.getClass().getMethod("frameBlock", $Block).invoke(builder, frameBlock)
-  builder.getClass().getMethod("lightWithItem", $Item).invoke(builder, lightItem)
+		// Force correct overloads
+		builder.getClass().getMethod("frameBlock", $Block).invoke(builder, frameBlock)
+		builder.getClass().getMethod("lightWithItem", $Item).invoke(builder, lightItem)
+		
+		const { R, G, B } = tintColorObj
 
-  builder
-    .destDimID(new $ResourceLocation("minecraft:the_nether"))
-    .returnDim(new $ResourceLocation("kubejs:crash_site"), false)
-    .tintColor(45,65,101)
-    .registerPortal()
+		builder
+			.destDimID(new $ResourceLocation(dimDestination))
+			.returnDim(new $ResourceLocation(dimReturn), false)
+			.tintColor(R, G, B)
+			.registerPortal()
 
-  console.log("Custom portal registered using custom item to light it.")
+		console.log("Custom portal registered using custom item to light it.")
+	}
+	
+	customPortal(
+		"minecraft:netherrack",
+		"minecraft:blaze_powder",
+		"minecraft:the_nether",
+		"kubejs:crash_site", 
+		{ R: 255, G: 0, B: 0 }
+	)
 })
