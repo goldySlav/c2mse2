9fa7eb8fce1b2f2fab7dc34f24faa28c7e39fc4f
goldySlav <g00L1v3rjkE@proton.me>
2025-04-08 14:45:58 +0200

    fix for villager trades: only modded villager trades are disabled, lunarian villager and lunarian wandering trader are disabled

---
+++ kubejs/server_scripts/trades.js
@@ -0,0 +1,12 @@
+// priority: 0
+
+MoreJSEvents.villagerTrades(event => {
+    event.removeModdedTrades();
+});
+
+MoreJSEvents.wandererTrades(event => {
+    //event.removeVanillaTrades(1);
+    //event.removeVanillaTrades(2);
+    //event.removeModdedTrades(1);
+    //event.removeModdedTrades(2);
+});
