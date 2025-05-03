1680d109b0053098504e19f78f3f3194cb03fa00
goldySlav <g00L1v3rjkE@proton.me>
2025-05-03 20:42:27 +0200

    rebalance netherite & emerald-netherite recipes

--- kubejs/server_scripts/mods/minecraft/armors/netherite_from_emerald.js
+++ kubejs/server_scripts/mods/minecraft/armors/netherite_from_emerald.js
@@ -5,6 +5,43 @@ ServerEvents.recipes(e => {
 		removeRecipeByID(e, [
 			MC(`netherite_${piece}_smithing`)
 		])
-		e.smithing(MC(`netherite_${piece}`), PET(`pure_emerald_armor_${piece}`), MC("netherite_ingot"))
+	})
+
+	//helmet
+	shapedSaveNBT(e, MC('netherite_helmet'), [
+		'BBB',
+		'BAB',
+	], {
+		A: PET('pure_emerald_armor_helmet'),
+		B: F("#ingots/netherite"),
+	})
+
+	//chestplate
+	shapedSaveNBT(e, MC('netherite_chestplate'), [
+		'BAB',
+		'BBB',
+		'BBB',
+	], {
+		A: PET('pure_emerald_armor_chestplate'),
+		B: F("#ingots/netherite"),
+	})
+
+	//leggings
+	shapedSaveNBT(e, MC('netherite_leggings'), [
+		'BBB',
+		'BAB',
+		'B B'
+	], {
+		A: PET('pure_emerald_armor_leggings'),
+		B: F("#ingots/netherite"),
+	})
+
+	//boots
+	shapedSaveNBT(e, MC('netherite_boots'), [
+		'BAB',
+		'B B'
+	], {
+		A: PET('pure_emerald_armor_boots'),
+		B: F("#ingots/netherite"),
 	})
 })
