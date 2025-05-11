e6bf1388dee3b2ed9d72ea4f1d8f9eec70f3708d
goldySlav <g00L1v3rjkE@proton.me>
2025-05-11 13:41:32 +0200

    rename bucket >> iron bucket

--- kubejs/client_scripts/renaming_items.js
+++ kubejs/client_scripts/renaming_items.js
@@ -33,6 +33,7 @@ ClientEvents.lang('en_us', event => {
 	event.renameBlock('create:creative_fluid_tank', "Creative Fluid Tank (Vertical)")
 	
 	event.renameItem('minecraft:shield', "Iron Shield")
+	event.renameItem('minecraft:bucket', "Iron Bucket")
 	
 	event.renameBlock('createsifter:crushed_end_stone', "Crushed End Stone")
 	event.renameBlock('createsifter:dust', "Dust Block")
